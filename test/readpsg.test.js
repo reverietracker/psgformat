c1a854dd30cd3928e07c4bce4d07ca2f374a91c3
Matt Westcott <matt@west.co.tt>
2023-08-24 21:26:57 +0200

    complete test coverage

--- test/readpsg.test.js
+++ test/readpsg.test.js
@@ -22,3 +22,32 @@ test("can read PSG files", () => {
         [ 9, 15 ],  [ 10, 15 ]
     ]);
 });
+
+test("can read PSG files with end markers", () => {
+    // apparently the end marker is kind of optional, and the psg files
+    // created by ay-emul don't have them. So hack this one so it does...
+    const buf = fs.readFileSync(path.join(__dirname, 'paraend.psg'));
+    buf[buf.length - 1] = 0xfd;
+    const frames = readPSG(buf);
+    expect(frames.length).toBe(1968);
+    expect(frames[0]).toStrictEqual([
+        [ 0, 0 ],  [ 1, 0 ],
+        [ 2, 0 ],  [ 3, 0 ],
+        [ 4, 0 ],  [ 5, 0 ],
+        [ 6, 0 ],  [ 7, 0 ],
+        [ 8, 0 ],  [ 9, 0 ],
+        [ 10, 0 ], [ 11, 0 ]
+    ]);
+    expect(frames[1]).toStrictEqual([
+        [ 0, 248 ], [ 1, 1 ],
+        [ 2, 223 ], [ 3, 1 ],
+        [ 4, 253 ], [ 5, 3 ],
+        [ 7, 40 ],  [ 8, 15 ],
+        [ 9, 15 ],  [ 10, 15 ]
+    ]);
+});
+
+test("raises exception on invalid PSG files", () => {
+    const buf = Buffer.from('Not a PSG file');
+    expect(() => readPSG(buf)).toThrow("File is not a valid PSG file");
+});
--- test/writepsg.test.js
+++ test/writepsg.test.js
@@ -3,7 +3,7 @@ const { writePSG } = require('..');
 
 test("can write PSG files", () => {
     const writer = new streams.WritableStream();
-    writePSG(writer, [
+    const frames = [
         [ [0, 0], [1, 11], [2, 22] ],
         [ ],
         [ [1, 0] ],
@@ -12,10 +12,17 @@ test("can write PSG files", () => {
         [ ],
         [ ],
         [ [2, 0] ],
-    ])
+    ];
+    for (let i = 0; i < 2000; i++) {
+        frames.push([]);
+    }
+    frames.push([ [1, 99] ]);
+    writePSG(writer, frames);
     const buf = writer.toBuffer();
     const header = buf.toString('latin1', 0, 16);
     expect(header).toBe('PSG\x1a\0\0\0\0\0\0\0\0\0\0\0\0');
     const data = buf.subarray(16);
-    expect(data).toStrictEqual(Buffer.from([0, 0, 1, 11, 2, 22, 0xff, 0xff, 1, 0, 0xfe, 1, 0xff, 2, 0, 0xff]));
+    expect(data).toStrictEqual(
+        Buffer.from([0, 0, 1, 11, 2, 22, 0xff, 0xff, 1, 0, 0xfe, 1, 0xff, 2, 0, 0xfe, 255, 0xfe, 245, 0xff, 1, 99, 0xff])
+    );
 });
